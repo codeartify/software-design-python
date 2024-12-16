6b7c78f417c6bce08063fe9e0dcfde58eafea4cc
Oliver Zihler <github@codeartify.com>
2024-12-16 16:48:04 +0100

    solution

--- shapes/circle.py
+++ shapes/circle.py
@@ -14,28 +14,25 @@ class Circle(Shape):
         self.y = y
         self.r = r
         self.color = Color(Color.GREEN)
-        self.number_of_contained_points = 0
 
     def count_contained_points(self, x_coords, y_coords):
-        self.number_of_contained_points = 0
         if x_coords is None or len(x_coords) <= 0:
             raise ValueError("x coordinates are empty")
         if y_coords is None or len(y_coords) <= 0:
             raise ValueError("y coordinates are empty")
         if len(x_coords) != len(y_coords):
             raise ValueError("Not every provided x coordinate has a matching y coordinate")
-            # check if the point is inside the circle
+        number_of_contained_points = 0
         for i in range(len(x_coords)):
-            self.contains(x_coords, y_coords, i)
-        return self.number_of_contained_points
+            if self.contains(x_coords, y_coords, i):
+                number_of_contained_points += 1
+        return number_of_contained_points
 
     def contains(self, x_coords, y_coords, i):
+        # check if the point is inside the circle
         delta_x = x_coords[i] - self.x
         delta_y = y_coords[i] - self.y
-        result = square(delta_x) + square(delta_y) <= square(self.r)
-        if result:
-            self.number_of_contained_points += 1
-        return result
+        return square(delta_x) + square(delta_y) <= square(self.r)
 
     def move_to(self, x, y):
         self.x = x
